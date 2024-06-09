e4d0d33dcb8c8010657d653a6052ea3802fa95f5
RYN BSD <rynbsd04@gmail.com>
2024-06-09 21:01:48 +0100

    add generics

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,5 +1,3 @@
-/// <reference types="node" resolution-mode="require"/>
-/// <reference types="node" resolution-mode="require"/>
 import type { IsOptions } from "./types.js";
 import * as Node from "./node.js";
 import * as Web from "./web.js";
@@ -8,20 +6,20 @@ export declare const typeFn: typeof Web.type | typeof Node.type;
 export type InputTp = typeof isBrowser extends true ? Web.TypeInput : Node.TypeInput;
 export type TypeFn = typeof isBrowser extends true ? typeof Web.type : typeof Node.type;
 declare const _default: {
-    readonly isApplication: (input: InputTp | InputTp[], options?: IsOptions) => Promise<boolean | import("./types.js").Return<string | import("stream").Readable | Uint8Array | ArrayBuffer | Buffer>[]>;
-    readonly isImage: (input: InputTp | InputTp[], options?: IsOptions) => Promise<boolean | import("./types.js").Return<string | import("stream").Readable | Uint8Array | ArrayBuffer | Buffer>[]>;
-    readonly isVideo: (input: InputTp | InputTp[], options?: IsOptions) => Promise<boolean | import("./types.js").Return<string | import("stream").Readable | Uint8Array | ArrayBuffer | Buffer>[]>;
-    readonly isAudio: (input: InputTp | InputTp[], options?: IsOptions) => Promise<boolean | import("./types.js").Return<string | import("stream").Readable | Uint8Array | ArrayBuffer | Buffer>[]>;
-    readonly isModel: (input: InputTp | InputTp[], options?: IsOptions) => Promise<boolean | import("./types.js").Return<string | import("stream").Readable | Uint8Array | ArrayBuffer | Buffer>[]>;
-    readonly isText: (input: InputTp | InputTp[], options?: IsOptions) => Promise<boolean | import("./types.js").Return<string | import("stream").Readable | Uint8Array | ArrayBuffer | Buffer>[]>;
-    readonly isFont: (input: InputTp | InputTp[], options?: IsOptions) => Promise<boolean | import("./types.js").Return<string | import("stream").Readable | Uint8Array | ArrayBuffer | Buffer>[]>;
+    readonly isApplication: <T extends Node.TypeInput | Node.TypeInput[]>(input: T, options?: IsOptions) => Promise<boolean | import("./types.js").Return<Node.TypeInput>[]>;
+    readonly isImage: <T_1 extends Node.TypeInput | Node.TypeInput[]>(input: T_1, options?: IsOptions) => Promise<boolean | import("./types.js").Return<Node.TypeInput>[]>;
+    readonly isVideo: <T_2 extends Node.TypeInput | Node.TypeInput[]>(input: T_2, options?: IsOptions) => Promise<boolean | import("./types.js").Return<Node.TypeInput>[]>;
+    readonly isAudio: <T_3 extends Node.TypeInput | Node.TypeInput[]>(input: T_3, options?: IsOptions) => Promise<boolean | import("./types.js").Return<Node.TypeInput>[]>;
+    readonly isModel: <T_4 extends Node.TypeInput | Node.TypeInput[]>(input: T_4, options?: IsOptions) => Promise<boolean | import("./types.js").Return<Node.TypeInput>[]>;
+    readonly isText: <T_5 extends Node.TypeInput | Node.TypeInput[]>(input: T_5, options?: IsOptions) => Promise<boolean | import("./types.js").Return<Node.TypeInput>[]>;
+    readonly isFont: <T_6 extends Node.TypeInput | Node.TypeInput[]>(input: T_6, options?: IsOptions) => Promise<boolean | import("./types.js").Return<Node.TypeInput>[]>;
     /**
      *
      * @param input - input path
      * @param me - mime or extension
      * @returns - is valid or not
      */
-    readonly isCustom: (input: InputTp | InputTp[], me: string, options?: IsOptions) => Promise<boolean | import("./types.js").Return<string | import("stream").Readable | Uint8Array | ArrayBuffer | Buffer>[]>;
+    readonly isCustom: <T_7 extends Node.TypeInput | Node.TypeInput[]>(input: T_7, me: string, options?: IsOptions) => Promise<boolean | import("./types.js").Return<Node.TypeInput>[]>;
 };
 export default _default;
 export type * from "./types.js";
--- src/index.ts
+++ src/index.ts
@@ -15,23 +15,29 @@ import {
 const isBrowser = typeof window !== "undefined";
 export const typeFn = isBrowser ? Web.type : Node.type;
 
-export type InputTp = typeof isBrowser extends true ? Web.TypeInput : Node.TypeInput;
-export type TypeFn = typeof isBrowser extends true ? typeof Web.type : typeof Node.type
+export type InputTp = typeof isBrowser extends true
+  ? Web.TypeInput
+  : Node.TypeInput;
+export type TypeFn = typeof isBrowser extends true
+  ? typeof Web.type
+  : typeof Node.type;
 
 export default {
-  isApplication: (input: InputTp | InputTp[], options?: IsOptions) =>
-    isApplication(typeFn as TypeFn, input, options),
-  isImage: (input: InputTp | InputTp[], options?: IsOptions) =>
+  isApplication: <T extends InputTp | InputTp[]>(
+    input: T,
+    options?: IsOptions
+  ) => isApplication(typeFn as TypeFn, input, options),
+  isImage: <T extends InputTp | InputTp[]>(input: T, options?: IsOptions) =>
     isImage(typeFn as TypeFn, input, options),
-  isVideo: (input: InputTp | InputTp[], options?: IsOptions) =>
+  isVideo: <T extends InputTp | InputTp[]>(input: T, options?: IsOptions) =>
     isVideo(typeFn as TypeFn, input, options),
-  isAudio: (input: InputTp | InputTp[], options?: IsOptions) =>
+  isAudio: <T extends InputTp | InputTp[]>(input: T, options?: IsOptions) =>
     isAudio(typeFn as TypeFn, input, options),
-  isModel: (input: InputTp | InputTp[], options?: IsOptions) =>
+  isModel: <T extends InputTp | InputTp[]>(input: T, options?: IsOptions) =>
     isModel(typeFn as TypeFn, input, options),
-  isText: (input: InputTp | InputTp[], options?: IsOptions) =>
+  isText: <T extends InputTp | InputTp[]>(input: T, options?: IsOptions) =>
     isText(typeFn as TypeFn, input, options),
-  isFont: (input: InputTp | InputTp[], options?: IsOptions) =>
+  isFont: <T extends InputTp | InputTp[]>(input: T, options?: IsOptions) =>
     isFont(typeFn as TypeFn, input, options),
   /**
    *
@@ -39,8 +45,11 @@ export default {
    * @param me - mime or extension
    * @returns - is valid or not
    */
-  isCustom: (input: InputTp | InputTp[], me: string, options?: IsOptions) =>
-    isCustom(typeFn as TypeFn, input, me, options),
+  isCustom: <T extends InputTp | InputTp[]>(
+    input: T,
+    me: string,
+    options?: IsOptions
+  ) => isCustom(typeFn as TypeFn, input, me, options),
 } as const;
 
 export type * from "./types.js";
